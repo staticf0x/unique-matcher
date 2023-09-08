bcf6e700920c287c65325d4d8050490e90361092
staticf0x <staticf0x@pm.me>
2023-09-08 13:32:47 +0200

    Fix match.py to be a useful debugging tool

--- match.py
+++ match.py
@@ -1,28 +1,41 @@
-import os
+import argparse
 import sys
 
 from devtools import debug
 from loguru import logger
+from PIL import Image
 
 from unique_matcher.matcher import Matcher
-
-templates: list[tuple] = []
-TEMPLATE_DIR = "generated"
-SCREENSHOT_DIR = "screenshots"
-THRESHOLD = 0.25
 
 logger.remove()
 logger.add(
     sys.stdout,
-    format="{time:YYYY-MM-DD HH:mm:ss} | <level>{level:7s}</level> | {message}",
-    level="INFO",
+    format="{time:YYYY-MM-DD HH:mm:ss.SSS} | <level>{level:7s}</level> | {message}",
+    level="DEBUG",
     colorize=True,
 )
 
+parser = argparse.ArgumentParser()
+parser.add_argument("screenshot", type=str, help="Path to the screenshot")
+parser.add_argument(
+    "--show-template",
+    action="store_true",
+    help="Display the item template that was used to match the item",
+)
+parser.add_argument("--check-one", type=str, help="Item name to check against")
+args = parser.parse_args()
+
 matcher = Matcher()
 
-for file in sorted(os.listdir(SCREENSHOT_DIR)):
-    result = matcher.find_item(os.path.join(SCREENSHOT_DIR, file))
+if args.check_one:
+    result = matcher.check_one(
+        Image.open(args.screenshot),
+        matcher.item_loader.get(args.check_one),
+    )
+else:
+    result = matcher.find_item(args.screenshot)
 
-    if result:
-        print(f"Found item {result.item.name} in {file}")
+debug(result)
+
+if args.show_template:
+    result.template.image.show()
