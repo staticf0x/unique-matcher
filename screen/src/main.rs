5027fd54c4e9467d149384473eafab657ab1451b
staticf0x <staticf0x@pm.me>
2023-09-17 12:08:37 +0200

    Auto-detect screen using PoE config file

--- screen/src/main.rs
+++ screen/src/main.rs
@@ -1,6 +1,8 @@
 use chrono::Local;
 use ini::Ini;
 use screenshots::Screen;
+use std::path::Path;
+use std::path::PathBuf;
 use std::{env, println};
 
 fn main() {
@@ -12,13 +14,25 @@ fn main() {
     let config_path_str = cfg_path.as_path().to_str().unwrap();
 
     // Load config
-    let screen_id: usize = match Ini::load_from_file(config_path_str) {
+    let mut screen_id: i32 = match Ini::load_from_file(config_path_str) {
         Ok(ini_file) => ini_file
             .get_from_or(Some("screenshot"), "screen", "0")
-            .parse::<usize>()
+            .parse::<i32>()
             .unwrap(),
-        Err(_) => 0 as usize,
+        Err(_) => 0 as i32,
     };
+
+    if screen_id == -1 {
+        // Auto-detect
+        let poe_config_path = poe_config_path().unwrap();
+        screen_id = match active_monitor_number_from_poe_config(poe_config_path) {
+            Some(val) => val as i32,
+            None => {
+                println!("Couldn't auto-detect PoE screen, defaulting to 0");
+                0
+            }
+        }
+    }
 
     println!("Using screen ID: {}", screen_id);
 
@@ -33,7 +47,7 @@ fn main() {
     let screens = Screen::all().unwrap();
 
     // Use the first one
-    if screen_id >= screens.len() {
+    if screen_id as usize >= screens.len() {
         println!(
             "Error: Cannot use screen {}, you only have {} screen(s) (IDs go from 0 to {})",
             screen_id,
@@ -41,7 +55,7 @@ fn main() {
             screens.len() - 1,
         );
     } else {
-        let screen = screens[screen_id];
+        let screen = screens[screen_id as usize];
 
         // Make screenshot
         let image = screen.capture().unwrap();
@@ -49,4 +63,53 @@ fn main() {
 
         println!("Screenshot saved to: {}", &image_path.to_str().unwrap());
     }
+}
+
+/// cross-platform C:\Users\username\Documents\My Games\Path of Exile\production_Config.ini
+fn poe_config_path() -> Option<PathBuf> {
+    dirs_next::document_dir().map(|docs_dir| {
+        docs_dir
+            .join("My Games")
+            .join("Path of Exile")
+            .join("production_Config.ini")
+    })
+}
+
+/// Read poe production_Config.ini, try to find the index of the preferred minitor.
+/// Something like this:
+///
+///  adapter_name=AMD Radeon RX 5700 XT(#0)
+///
+fn active_monitor_number_from_poe_config<P>(poe_config_path: P) -> Option<usize>
+where
+    P: AsRef<Path>,
+{
+    let poe_config_path = poe_config_path.as_ref();
+
+    println!("{:?}", poe_config_path);
+
+    if !poe_config_path.exists() {
+        println!("PoE config path doesn't exist (are you on Linux?)");
+        return None;
+    }
+
+    let ini_file = Ini::load_from_file(poe_config_path.to_str().unwrap()).unwrap();
+
+    let adapter_name = ini_file.get_from_or(Some("DISPLAY"), "adapter_name", "(#0)");
+
+    let Some(start) = adapter_name.rfind("(#") else {
+        return None;
+    };
+
+    let Some(end) = adapter_name.rfind(")") else {
+        return None;
+    };
+
+    let Some(substr) = adapter_name.get(start + 2..end) else {
+        return None;
+    };
+
+    let monitor_index = substr.parse::<usize>().ok()?;
+
+    Some(monitor_index)
 }
