efcd410da8bfe26808c2786be9eaa3e3fdc21b48
staticf0x <staticf0x@pm.me>
2024-03-25 19:44:39 +0100

    Revert "Remove test for item dimensions"
    This reverts commit b257f2fa537a03652c6493d3abdf14e2225543bc.

---
+++ tests/test_item_db.py
@@ -0,0 +1,16 @@
+import warnings
+
+def test_item_base_dimensions(item_loader):
+    """Test that all items within one base have the same dimension."""
+    bases = {}
+
+    # Group items by base
+    for item in item_loader:
+        bases.setdefault(item.base, [])
+        bases[item.base].append(item)
+
+    for base, items in bases.items():
+        msg = f"Items from base '{base}' don't have the same dimensions"
+
+        if len({(item.width, item.height) for item in items}) != 1:
+            warnings.warn(msg, stacklevel=1)
